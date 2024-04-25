e378810a8d79297cffbb34498ef923633898e368
AtsukiTakahashi <takatomgoo@gmail.com>
2024-04-25 19:52:39 +0900

    branch命令の実装

--- core/controller.sv
+++ core/controller.sv
@@ -9,7 +9,7 @@ module controller(
   input wire [6:0] funct7,
   input wire [11:0] imm_i,
   input wire [11:0] imm_s,
-  input wire [12:1] imm_b,
+  input wire [11:0] imm_b,
   input wire [31:0] rs1_rd,
   input wire [31:0] rs2_rd,
   input wire [31:0] alu_out,
@@ -25,15 +25,17 @@ module controller(
   output logic [31:0] rd_wd,
   output logic [31:0] pc_next
 );
-  wire [31:0] sext_imm_i = {{20{imm_i[11]}}, imm_i};
-  wire [31:0] sext_imm_s = {{20{imm_s[11]}}, imm_s};
+  wire [31:0] sign_ext_imm_i = {{20{imm_i[11]}}, imm_i};
+  wire [31:0] sign_ext_imm_s = {{20{imm_s[11]}}, imm_s};
+  wire [31:0] sign_ext_imm_b = {{19{imm_b[11]}}, imm_b, 1'b0};
+  wire [31:0] branch_addr = pc + sign_ext_imm_b;
 
   always_comb begin
     case(opcode)
       7'b0000011: begin // Load Ops (I-Type)
         alu_op = ALU_ADD;
         alu_src_a = rs1_rd;
-        alu_src_b = sext_imm_i;
+        alu_src_b = sign_ext_imm_i;
         mem_addr = alu_out;
         rd_we = 1;
         rd_wd = mem_out;
@@ -44,7 +46,7 @@ module controller(
       7'b0100011: begin // Store Ops (S-Type)
         alu_op = ALU_ADD;
         alu_src_a = rs1_rd;
-        alu_src_b = sext_imm_s;
+        alu_src_b = sign_ext_imm_s;
         mem_addr = alu_out;
         mem_we = 1;
         mem_wd = rs2_rd;
@@ -53,139 +55,95 @@ module controller(
         pc_next = pc + 4;
       end
       7'b0010011: begin // Imm ALU Ops (I-Type)
+        mem_addr = 0;
+        mem_we = 0;
+        mem_wd = 0;
+        pc_next = pc + 4;
         case (funct3)
           3'b000: begin // addi
             alu_op = ALU_ADD;
             alu_src_a = rs1_rd;
-            alu_src_b = sext_imm_i;
+            alu_src_b = sign_ext_imm_i;
             rd_we = 1;
             rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
           end
         endcase
       end
       7'b0110011: begin // ALU Ops (R-Type)
+        alu_src_a = rs1_rd;
+        alu_src_b = rs2_rd;
+        rd_we = 1;
+        rd_wd = alu_out;
+        mem_addr = 0;
+        mem_we = 0;
+        mem_wd = 0;
+        pc_next = pc + 4;
         case (funct3)
           3'b000: begin
             case (funct7)
-              7'b0000000: begin // add
-                alu_op = ALU_ADD;
-                alu_src_a = rs1_rd;
-                alu_src_b = rs2_rd;
-                rd_we = 1;
-                rd_wd = alu_out;
-                mem_addr = 0;
-                mem_we = 0;
-                mem_wd = 0;
-                pc_next = pc + 4;
-              end
-              7'b0100000: begin // sub
-                alu_op = ALU_SUB;
-                alu_src_a = rs1_rd;
-                alu_src_b = rs2_rd;
-                rd_we = 1;
-                rd_wd = alu_out;
-                mem_addr = 0;
-                mem_we = 0;
-                mem_wd = 0;
-                pc_next = pc + 4;
-              end
+              7'b0000000: alu_op = ALU_ADD;
+              7'b0100000: alu_op = ALU_SUB;
             endcase
           end
-          3'b001: begin // sll
-            alu_op = ALU_SLL;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
-          end
-          3'b010: begin // slt
-            alu_op = ALU_SLT;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
-          end
-          3'b011: begin // sltu
-            alu_op = ALU_SLTU;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
-          end
-          3'b100: begin // xor
-            alu_op = ALU_XOR;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
-          end
+          3'b001: alu_op = ALU_SLL;
+          3'b010: alu_op = ALU_SLT;
+          3'b011: alu_op = ALU_SLTU;
+          3'b100: alu_op = ALU_XOR;
           3'b101: begin
             case (funct7)
-              7'b0000000: begin // srl
-                alu_op = ALU_SRL;
-                alu_src_a = rs1_rd;
-                alu_src_b = rs2_rd;
-                rd_we = 1;
-                rd_wd = alu_out;
-                mem_addr = 0;
-                mem_we = 0;
-                mem_wd = 0;
-                pc_next = pc + 4;
-              end
-              7'b0100000: begin // sra
-                alu_op = ALU_SRA;
-                alu_src_a = rs1_rd;
-                alu_src_b = rs2_rd;
-                rd_we = 1;
-                rd_wd = alu_out;
-                mem_addr = 0;
-                mem_we = 0;
-                mem_wd = 0;
-                pc_next = pc + 4;
-              end
+              7'b0000000: alu_op = ALU_SRL;
+              7'b0100000: alu_op = ALU_SRA;
             endcase
           end
-          3'b110: begin // or
-            alu_op = ALU_OR;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
+          3'b110: alu_op = ALU_OR;
+          3'b111: alu_op = ALU_AND;
+        endcase
+      end
+      7'b1100011: begin // Branch Ops (B-Type)
+        pc_next = pc + 4;
+        alu_src_a = rs1_rd;
+        alu_src_b = rs2_rd;
+        rd_we = 0;
+        rd_wd = 0;
+        mem_addr = 0;
+        mem_we = 0;
+        mem_wd = 0;
+        case (funct3)
+          3'b000: begin // beq
+            alu_op = ALU_SUB;
+            if (alu_out == 0) begin
+              pc_next = branch_addr;
+            end
           end
-          3'b111: begin // and
-            alu_op = ALU_AND;
-            alu_src_a = rs1_rd;
-            alu_src_b = rs2_rd;
-            rd_we = 1;
-            rd_wd = alu_out;
-            mem_addr = 0;
-            mem_we = 0;
-            mem_wd = 0;
-            pc_next = pc + 4;
+          3'b001: begin // bne
+            alu_op = ALU_SUB;
+            if (alu_out != 0) begin
+              pc_next = branch_addr;
+            end
+          end
+          3'b100: begin // blt
+            alu_op = ALU_SLT;
+            if (alu_out == 1) begin
+              pc_next = branch_addr;
+            end
+          end
+          3'b101: begin // bge
+            alu_op = ALU_SLT;
+            if (alu_out == 0) begin
+              pc_next = branch_addr;
+            end
+          end
+          3'b110: begin // bltu
+            alu_op = ALU_SLTU;
+            if (alu_out == 1) begin
+              pc_next = branch_addr;
+            end
+          end
+          3'b111: begin // bgeu
+            alu_op = ALU_SLTU;
+            if (alu_out == 0) begin
+              pc_next = branch_addr;
+            end
           end
         endcase
       end
